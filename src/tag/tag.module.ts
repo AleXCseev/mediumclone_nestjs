ed077a2b1191257bc24d446a84a2a4b3b2cca361
Kontantin Alekseev <alexcseev1990@gmail.com>
2022-10-26 17:31:13 +0300

    fix

---
+++ src/tag/tag.module.ts
@@ -0,0 +1,12 @@
+import { TagService } from './tag.service';
+import { TagController } from './tag.controller';
+import { Module } from "@nestjs/common";
+
+@Module({
+    controllers: [TagController],
+    providers: [TagService],
+
+})
+export class TagModule {
+
+}
